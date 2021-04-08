81568510470b183be43073fb2005356b434ccf3c
kranjann <krishna.ranjan.2021@gmail.com>
2021-04-09 01:51:29 +0530

    updated

---
+++ test_demo_one.py
@@ -0,0 +1,9 @@
+# any pytest file should start with test_
+# pytest method name should start with test
+# any code should be wrapped in a method
+
+import pytest
+
+def test_firstProgram():
+    print("You have invoked first program")
+
---
+++ test_demo_two.py
@@ -0,0 +1,5 @@
+
+
+def test_secondProgram():
+    msg = "Hello second program"
+    assert msg == "Hi, test failed because assertion failed"
